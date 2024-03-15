33b0ef9a64c3d2ba44efcf7d5f201605900d916f
paul-philipp.jacobs <paul-philipp.jacobs@medizin.uni-leipzig.de>
2024-03-15 16:05:33 +0100

    removed logging

--- racoons/config.py
+++ racoons/config.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import yaml
-from racoons import logger
 
 
 def configurable(func):
@@ -9,14 +8,12 @@ def configurable(func):
         """Injects configuration keywords."""
         config = kwargs.get("config")
         if config is None:
-            logger.info("No config file loaded. Use default parameters.")
             return func(*args, **kwargs)
         elif isinstance(config, dict):
-            logger.info("Using configs from dict")
+            print(f"Use config from dict")
         else:
             with open(config, 'r') as f:
                 config = yaml.safe_load(f)
-            logger.info(f"Loaded configuration file {config}")
         fname = Path(func.__globals__['__file__']).name
         conf = config[fname][func.__qualname__]
         conf.update(kwargs)
